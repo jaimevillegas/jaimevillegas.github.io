7778bb14baf6b9642b53d34501ffcb4b211ecd5c
Jaime Villegas <jaimevillegas296@gmail.com>
2023-06-21 20:06:41 -0500

    Add floating nav

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -18,9 +18,10 @@ const Header = () => {
             <img src={ME} alt="me" />
           </div>
 
+          <a href="#contact" className='scroll__down'>Scroll Down</a>
+
           <HeaderSocials />
 
-          <a href="#contact" className='scroll__down'>Scroll Down</a>
 
         </div>
       </div>
