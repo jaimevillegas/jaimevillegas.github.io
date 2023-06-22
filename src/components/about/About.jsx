e65247b257c0e93bc50ae2060a3ae019026ca04e
Jaime Villegas <jaimevillegas296@gmail.com>
2023-06-21 20:31:49 -0500

    Work on About Me section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { LuFolderCheck } from 'react-icons/lu'
 
 const About = () => {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      
+      <div className="conainer about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="about__me image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>7+ Years Working</small>
+            </article>
+
+            <article className="about__card">
+              <FiUsers className='about__icon' />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+
+            <article className="about__card">
+              <LuFolderCheck className='about__icon' />
+              <h5>Projects</h5>
+              <small>80+ Completed Projects</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio, enim? Sed neque soluta reiciendis beatae temporibus ea perspiciatis praesentium dolore autem sequi nesciunt, vero voluptas veniam nobis suscipit omnis iusto?
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+
+        </div>
+      </div>
+    </section>
   )
 }
 
