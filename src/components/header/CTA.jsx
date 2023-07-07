7f47633a81fe94e49978fb852036ff71b7828b18
Jaime Villegas <jaimevillegas296@gmail.com>
2023-07-07 15:12:32 -0500

    Make header multilingual

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import CV from '../../assets/resume.pdf'
+import { useTranslation } from 'react-i18next'
 
 const CTA = () => {
+  const [t, i18n] = useTranslation('global');
+
   return (
     <div className='cta'>
-      <a href={CV} download className='btn'>Download CV</a>
-      <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+      <a href={CV} download className='btn'>{t("CTA.CV")}</a>
+      <a href="#contact" className='btn btn-primary'>{t("CTA.contact")}</a>
     </div>
   )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,22 +3,33 @@ import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me-1.png'
 import HeaderSocials from './HeaderSocials'
+import { useTranslation } from 'react-i18next'
 
 const Header = () => {
+  const [t, i18n] = useTranslation('global');
+
+  const handleChangeLanguage = (lang) => {
+    i18n.changeLanguage(lang);
+  };
+
   return (
     <header>
       <div className="container">
         <div className="header__container">
-          <h5>Hello, I'm</h5>
+          <h5>{t("header.h5")}</h5>
           <h1>Jaime Villegas</h1>
-          <h5 className="text-light">A Full-Stack Web Developer and Mechatronics Engineer</h5>
+
+          <button onClick={() => handleChangeLanguage('en')}>EN</button>
+          <button onClick={() => handleChangeLanguage('es')}>ES</button>
+          <h5 className="text-light">{t("header.description")}</h5>
           <CTA />
 
           <div className="me">
             <img src={ME} alt="me" />
           </div>
 
-          <a href="#contact" className='scroll__down'>Scroll Down</a>
+          {/* <a href="#contact" className='scroll__down'>Scroll Down</a> */}
+          <a href="#contact" className='scroll__down'>{t("header.scroll")}</a>
 
           <HeaderSocials />
 
