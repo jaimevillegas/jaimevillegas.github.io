af2197ae8eed00389225789d5ba518c783f11b11
Jaime Villegas <jaimevillegas296@gmail.com>
2023-07-12 17:38:57 -0500

    Work on Services Translation

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,37 +1,31 @@
 import React from 'react'
 import './services.css'
 import { BiCheck } from 'react-icons/bi'
+import { useTranslation } from 'react-i18next'
 
 const Services = () => {
+  const [t, i18n] = useTranslation('global');
   return (
     <section id='services'>
-      <h5>What I Offer</h5>
-      <h2>Services</h2>
+      <h5>{t("services.titleSmall")}</h5>
+      <h2>{t("services.titleBig")}</h2>
       <div className="container services__container">
         <article className="service">
           <div className="service__head">
-            <h3>Web Development</h3>
+            <h3>{t("services.webDevTitle")}</h3>
           </div>
           <ul className='service__list'>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p><b>Responsive Web Design:</b> Creation of websites that are optimized for various devices and screen sizes, providing a seamless user experience.</p>
+              <p>{t("services.webDevService1")}</p>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p><b>Front-End Development:</b> Build interactive and visually appealing user interfaces using HTML, CSS, JavaScript and React.</p>
+              <p>{t("services.webDevService2")}</p>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p><b>Back-End Development:</b> Build robust server-side logic and databases using Ruby on Rails and PostgreSQL.</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p><b>API Development and Integration:</b> Build and integrate APIs (Application Programming Interfaces) to enable efficient data exchange and communication between different systems.</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p><b>Website Maintenance and Support:</b> Provide reliable website maintenance, bug fixes, security updates, and technical support to ensure the ongoing smooth operation of websites.</p>
+              <p>{t("services.webDevService3")}</p>
             </li>
           </ul>
         </article>
@@ -39,56 +33,40 @@ const Services = () => {
 
         <article className="service">
           <div className="service__head">
-            <h3>CAD Design</h3>
+            <h3>{t("services.CADTitle")}</h3>
           </div>
           <ul className='service__list'>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p><b>2D Drafting:</b> Creation of accurate and detailed 2D technical drawings and plans.</p>
+              <p>{t("services.CADService1")}</p>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p><b>3D Modelling:</b> Creation of 3D models of machinery parts, products, mechanical parts and 3D printing models.</p>
+              <p>{t("services.CADService2")}</p>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p><b>Product Design:</b> Expertise in designing and developing new products using CAD software, ensuring functionality, aesthetics, and manufacturability.</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p><b>3D Rendering and visualization:</b> I can provide high-quality 3D renderings and visualizations of CAD models, bringing designs to life and helping clients visualize the final product.</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p><b>Technical Drawing Generation for Manufacturing:</b> I specialize in creating detailed technical drawings and documentation that are ready for manufacturing processes. This includes producing accurate dimensions, annotations, tolerances, and specifications required by manufacturers to produce the designed product. </p>
+              <p>{t("services.CADService3")}</p>
             </li>
           </ul>
         </article>
 
         <article className="service">
           <div className="service__head">
-            <h3>Mechatronics</h3>
+            <h3>{t("services.MechTitle")}</h3>
           </div>
           <ul className='service__list'>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p><b>System Design and Integration:</b> I specialize in designing and integrating complex mechatronic systems by combining mechanical, electrical, and control engineering principles.</p>
+              <p>{t("services.MechService1")}</p>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p><b>Robotics and Automation:</b> I possess expertise in designing and developing robotic systems and automation solutions for various industries, optimizing efficiency and productivity.</p>
+              <p>{t("services.MechService2")}</p>
             </li>
             <li>
               <BiCheck className='service__list-icon'/>
-              <p><b>Sensor Integration and Data Acquisition:</b> I have experience in integrating sensors and acquiring data from various sources, enabling real-time monitoring and feedback for precise control.</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p><b>Embedded Systems Development:</b> I offer services in developing embedded systems for mechatronic applications, including microcontroller programming, firmware development, and interfacing with sensors and actuators.</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon'/>
-              <p><b>Industrial IoT (Internet of Things):</b> I possess knowledge in implementing IoT solutions for industrial applications, enabling connectivity, data analysis, and remote monitoring of mechatronic systems.</p>
+              <p>{t("services.MechService3")}</p>
             </li>
           </ul>
         </article>
