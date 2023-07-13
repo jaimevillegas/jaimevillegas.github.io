83ede00d1f91264f3f66aa4ce2bad160df0af764
Jaime Villegas <jaimevillegas296@gmail.com>
2023-07-13 16:53:45 -0500

    Add translation to Testimonials component

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './testimonials.css'
 import AVTR1 from '../../assets/avatar1.jpg'
 import AVTR2 from '../../assets/avatar2.jpg'
+import { useTranslation } from 'react-i18next'
 
 // import Swiper core and required modules
 import { Pagination} from 'swiper';
@@ -13,24 +14,25 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 
 
-const data = [
-  {
-    avatar: AVTR1,
-    name: 'Shayan Ali Bakhsh',
-    review: "Jaime is an exceptional software developer whom I had the privilege of working with on a web application. Jaime's attention to detail and problem-solving skills were truly impressive, and he consistently delivered high-quality work. He is a dedicated and driven individual who always goes above and beyond to ensure the success of his team. Jaime is also a friendly and supportive colleague who is always willing to help others and share his knowledge. It was a pleasure to work with Jaime, and I would highly recommend him for any software development project that demands expertise, passion, and a collaborative mindset."
-  },
-  {
-    avatar: AVTR2,
-    name: 'Nicolas Andres Olaya',
-    review: 'I highly recommend my friend as a Full Stack Developer. He is extremely punctual, respectful, and hardworking. He has a strong understanding of programming languages and frameworks, and is dedicated to constantly improving his skills. He is a great team player and always willing to lend a hand. I have no doubt that he will be an asset to any company or project he works on.'
-  }
-]
 
 const Testimonials = () => {
+  const [t, i18n] = useTranslation('global');
+  const data = [
+    {
+      avatar: AVTR1,
+      name: 'Shayan Ali Bakhsh',
+      review: `${t('testimonials.testimonial1Description')}`
+    },
+    {
+      avatar: AVTR2,
+      name: 'Nicolas Andres Olaya',
+      review: `${t('testimonials.testimonial2Description')}`
+    }
+  ]
   return (
     <section id='testimonials'>
-      <h5>Review from clients</h5>
-      <h2>Testimonials</h2>
+      <h5>{t("testimonials.titleSmall")}</h5>
+      <h2>{t("testimonials.titleBig")}</h2>
 
       <Swiper 
         className="container testimonials__container"
